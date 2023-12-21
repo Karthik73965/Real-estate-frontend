2c72f6a69ec204e817973d7f2585664a6de17203
Karthik73965 <karthikjidagam81@gmail.com>
2023-12-21 05:00:55 -0800

    completed search bar functionality

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,27 @@
 import {FaSearch} from 'react-icons/fa'
-import {Link } from 'react-router-dom';
+import {Link, useNavigate } from 'react-router-dom';
 import {  useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
 
 export const Header = () => {
    const {currentUser }= useSelector((state)=>state.user)
+   const [searchTerm,setsearchTerm]=useState('')
+   const navigate = useNavigate()
    
+   const handlesubmit=(e)=>{
+         e.preventDefault();
+         const urlParams=new URLSearchParams(window.location.search)
+         urlParams.set('searchTerm',searchTerm);
+         const searchQuery = urlParams.toString();
+         navigate(`/search?${searchQuery}`)
+   }
+   useEffect(() => {
+      const urlParams = new URLSearchParams(location.search);
+      const searchTermFromUrl = urlParams.get('searchTerm');
+      if (searchTermFromUrl) {
+        setsearchTerm(searchTermFromUrl);
+      }
+    }, [location.search]);
   return (
     <header className='bg-slate-200 shadow-lg'>
        <div className='flex justify-between p-3 items-center max-w-6xl max-auto'>
@@ -14,9 +31,9 @@ export const Header = () => {
             <span className='text-slate-300'>Estates</span>
         </h1>
      </Link>
-        <form className=' flex bg-slate-100 p-3 rounded-xl items-center '>
-            <input type='text' placeholder='   search........' className='bg-transparent focus:outline-none w-24 sm:w-64  ' />
-            <FaSearch/>
+        <form onSubmit={handlesubmit} className=' flex bg-slate-100 p-3 rounded-xl items-center '>
+            <input type='text' placeholder='   search........' className='bg-transparent focus:outline-none w-24 sm:w-64  ' onChange={(e)=>setsearchTerm(e.target.value)}/>
+           <button> <FaSearch/></button>
         </form>
         <ul className='flex gap-4 justify-around'>
            <Link to ={'/'}> <li className='text-slate-800 hover:underline hover:text-black'>Home</li></Link>
